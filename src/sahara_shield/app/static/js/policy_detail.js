034c611a61bded9ca6b3c536c1474d380bcc6a53
ng-23 <23grattan@gmail.com>
2026-05-05 01:29:46 -0400

    Little frontend change

--- src/sahara_shield/app/static/js/policy_detail.js
+++ src/sahara_shield/app/static/js/policy_detail.js
@@ -93,6 +93,7 @@ async function loadPolicyDetail() {
       ['HTTP Method', policy.http_method],
       ['Route Pattern', policy.route_pattern],
       ['Mode', policy.mode],
+      ['Decision Engine', policy.decision_engine_key],
       ['Status', policy.active ? 'Active' : 'Inactive'],
       ['Priority', policy.priority],
       ['Action Score Threshold', policy.action_score_threshold],
